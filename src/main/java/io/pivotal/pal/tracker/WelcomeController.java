eb32b440fa4ba560d1d0d8f2abe09a316bf4a76c
Yashwanth-B <yashwanth.boganadham@cognizant.com>
2020-04-21 19:58:26 +0530

    second task completion

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    String mess;
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.mess= message;
+    }
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "hello";
+    public String sayHello() {
+        return this.mess;
     }
 }
